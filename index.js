4cc73076f11a6e2cd0623fab2d427b501c47faa8
April Kiser <denise626@hotmail.com>
2018-05-01 23:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 
-function removeLastKitten(kittens) {
+function removeLastKitten() {
   kittens.slice(0, kittens.length -1)
   return kittens
 }
